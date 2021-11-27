b309c330f316408af883369861b5feebabc85fac
Felbf <felbf92@gmail.com>
2021-11-27 17:40:32 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const card = document.querySelector(".card");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSimbol = () => {
+  let simbols = ["diamond", "spade", "heart", "club"];
+  let indexSimbols = Math.floor(Math.random() * simbols.length);
+  return simbols[indexSimbols];
+};
+
+window.onload = () => {
+  card.classList.add(generateRandomSimbol());
+  card.innerHTML = generateRandomNumber();
 };
